a40eefb3df8597c3c033ca01d7bb2f378fcce981
LAPTOP-B3SC8PH8\Lydia George Michael <georgelydia42@gmail.com>
2024-03-13 15:41:24 +0200

    create onboarding screen

---
+++ lib/Screens/authentication/screens/login/login_screen.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:neat/Screens/authentication/screens/login/widgets/login_form.dart';
+import 'package:neat/Screens/authentication/screens/login/widgets/login_header.dart';
+import '../../../../common/styles/spacing_styles.dart';
+import '../../../../utlis/constants/colors.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: TColors.backgroundColor,
+      body: SingleChildScrollView(
+        child: Padding(padding: TSpacingStyle.paddingWithAppBarHeight,
+        child: Column(
+          children: [
+            /// Logo
+            LoginHeader(),
+
+            /// Form
+            LoginForm(),
+
+
+          ],
+        ),
+        ),
+      ),
+    );
+  }
+}
+
+
+
+
