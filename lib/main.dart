111d94233eb7a9eae924df72a8e44c0884eec0da
kyrolloss <111562259+kyrolloss@users.noreply.github.com>
2024-03-20 15:02:52 +0200

    handel notification logic

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,8 @@
 import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:neat/Screens/Task%20template%20Screen.dart';
+import 'package:neat/Screens/Notification/notification%20services/notification%20services.dart';
 import 'package:neat/cubit/app_cubit.dart';
-import 'Screens/Task template Screen.dart';
 import 'Screens/authentication/screens/onboarding/onboarding_screen.dart';
 import 'firebase_options.dart';
 final navigatorKey = GlobalKey<NavigatorState>();
@@ -15,9 +13,7 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-final fcmToken =  await FirebaseMessaging.instance.getToken();
-print('token is ${fcmToken}');
-
+await FirebaseApi().initNotification();
   runApp(const MyApp());
 }
 
